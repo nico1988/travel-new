2a67a690cb3903b50179b92ef50501bf2b340efb
nico1988 <410827024@qq.com>
2022-05-15 01:47:57 +0800

    feat: add

--- group/src/components/Calendar.js
+++ group/src/components/Calendar.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Modal, Button, Space, Card,notification, message } from 'antd';
-import { Input ,Drawer} from 'antd';
+import { Modal, Button, Space, Card, notification, message } from 'antd';
+import { Input, Drawer } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { Alert } from 'antd';
-import {Form} from 'antd';
+import { Form } from 'antd';
 import { UserOutlined, KeyOutlined, QuestionCircleFilled, LockOutlined, PushpinOutlined, CalendarOutlined } from '@ant-design/icons';
 import events from '../staticData/staticEvent';
 import { Calendar } from "react-big-calendar";
@@ -22,31 +22,31 @@ import { Refresh } from "@mui/icons-material";
 const localizer = momentLocalizer(moment)
 
 
-function EventCreater(props){
-    
+function EventCreater(props) {
+
     const [createForm] = Form.useForm()
     const [loading, setLoading] = useState(false);
-    const [FD,setFD] = useState(false)  // states of drawer
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" , location:""}); // new event
+    const [FD, setFD] = useState(false)  // states of drawer
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "", location: "" }); // new event
     const [allEvents, setAllEvents] = useState(events); // all event states
     const [isModalVisible, setIsModalVisible] = useState(false); // modal state
     const [formErrors, setformErrors] = useState(events); // form complete error state.
-    const [isSubmit,setissubmit] = useState(false) // submit click state.
+    const [isSubmit, setissubmit] = useState(false) // submit click state.
     const key = 'updatable';
     const history = useNavigate();
-    
+
 
     const loadEvents = async () => {
         const url = "/api/event/list";
         const result = await fetchGet(url);
-        
+
         // console.log(formErrors);
         const eve = result.data.events
-        console.log("this is event",eve)
-   
+        console.log("this is event", eve)
+
         let showingEvents = [];
         if (result.data && result.data.events && result.data.events.length > 0) {
-            result.data.events.map((e)=>{
+            result.data.events.map((e) => {
 
                 e.start = new Date(Date.parse(e.start))
                 e.end = new Date(Date.parse(e.end))
@@ -58,7 +58,7 @@ function EventCreater(props){
         }
         // console.log("all events are: ", showingEvents);
         setAllEvents(showingEvents)
-        
+
     }
 
     if (!loading) {
@@ -70,7 +70,7 @@ function EventCreater(props){
     const error = () => {
         message.error('Create event failed, please try again.');
     };
-    
+
     const showDrawer = () => {
         setFD(true);
     };
@@ -78,7 +78,7 @@ function EventCreater(props){
         setFD(false);
     };      //set state of drawer
 
-    const handlesubmit = async () =>{
+    const handlesubmit = async () => {
         setformErrors(validate(newEvent))
         setissubmit(true)
         console.log("new event is: ", newEvent);
@@ -91,58 +91,58 @@ function EventCreater(props){
             error();
         }
     }
-    
-    useEffect(()=>{
-        if(Object.keys(formErrors).length===0 && isSubmit){
+
+    useEffect(() => {
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
             setAllEvents([...allEvents, newEvent])
             console.log(newEvent.start)
         }
-    },[formErrors]) // if no form empty then set the new event
+    }, [formErrors]) // if no form empty then set the new event
 
-    const validate = (value) =>{
+    const validate = (value) => {
         const errors = {}
-        if (!value.title){
+        if (!value.title) {
             errors.title = "Title is required!"
         }
-        if (!value.location){
+        if (!value.location) {
             errors.location = "Location is required!"
         }
-        if (!value.start){
+        if (!value.start) {
             errors.start = "Start date is required!"
         }
-        if (!value.end){
+        if (!value.end) {
             errors.end = "End date is required!"
         }
         return errors
     }
 
     const openNotification = () => {
-        if(formErrors.titlen == ""){
+        if (formErrors.titlen == "") {
             notification.open({
                 key,
                 message: 'It seems that you miss Event detail! Will navigate to calendar after 2s. Check it and come back later!',
                 description: 'Warning!',
             })
-            
+
         }
-        else if(formErrors.locatioin == ""){
+        else if (formErrors.locatioin == "") {
             notification.open({
                 key,
                 message: 'It seems that you miss Event Location! Will navigate to calendar after 2s. Check it and come back later!',
                 description: 'Warning!',
             })
-            
+
         }
 
     }; // check form comeplete state and give warning or success message.
-    
+
     const showModal = () => {
         setIsModalVisible(true);
     };
 
     const handleOk = () => {
         setIsModalVisible(false);
-        
+
     };
 
     const handleCancel = (e) => {
@@ -152,16 +152,16 @@ function EventCreater(props){
     function resolveAfter2Seconds() {
         return new Promise(resolve => {
             setTimeout(() => {
-              resolve('resolved');
-              newEvent.start=''
-              newEvent.end=''
-              createForm.resetFields()
+                resolve('resolved');
+                newEvent.start = ''
+                newEvent.end = ''
+                createForm.resetFields()
             }, 2000);
-          });
+        });
     }
 
     async function asyncCall() {
-    
+
         const result = await resolveAfter2Seconds();
         showModal()
         onClose()
@@ -171,7 +171,7 @@ function EventCreater(props){
     }
 
 
-    return(
+    return (
         <div>
             <Homepage></Homepage>
 
@@ -179,81 +179,81 @@ function EventCreater(props){
                 <div>
                     <Card >
                         <Calendar localizer={localizer} events={allEvents}
-                        startAccessor="start" endAccessor="end" />
-                    </Card> 
+                            startAccessor="start" endAccessor="end" />
+                    </Card>
                 </div>
                 <div>
-                    <Drawer title="Create Event" placement="right" onClose={onClose} visible={FD} width={520} style={{zIndex:'999'}}>
+                    <Drawer title="Create Event" placement="right" onClose={onClose} visible={FD} width={520} style={{ zIndex: '999' }}>
                         <Card>
                             <Form onFinish={handlesubmit}
-                            autoComplete="off"
-                            form={createForm}
+                                autoComplete="off"
+                                form={createForm}
                             >
                                 <Form.Item
                                     name="title"
                                     rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please input Event Title!',
-                                    },
+                                        {
+                                            required: true,
+                                            message: 'Please input Event Title!',
+                                        },
                                     ]}
                                 >
-                                    <Input placeholder='Event Title' prefix={<UserOutlined className="site-form-item-icon" />} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value})} />
+                                    <Input placeholder='Event Title' prefix={<UserOutlined className="site-form-item-icon" />} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
                                 </Form.Item>
 
                                 <Form.Item
                                     name="location"
                                     rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please input Event Location!',
-                                    },
+                                        {
+                                            required: true,
+                                            message: 'Please input Event Location!',
+                                        },
                                     ]}
                                 >
-                                    <Input placeholder='Event Location' prefix={<PushpinOutlined className="site-form-item-icon" />} onChange={(e) => setNewEvent({ ...newEvent, location: e.target.value})} />
+                                    <Input placeholder='Event Location' prefix={<PushpinOutlined className="site-form-item-icon" />} onChange={(e) => setNewEvent({ ...newEvent, location: e.target.value })} />
                                 </Form.Item>
 
                                 <Form.Item
                                     name="startDate"
                                     rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please select Event Start Date!',
-                                    },
+                                        {
+                                            required: true,
+                                            message: 'Please select Event Start Date!',
+                                        },
                                     ]}
                                 >
-                                    <DatePicker placeholderText="Start Date:"  selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })}  className='datePickBox'/>
+                                    <DatePicker placeholderText="Start Date:" selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} className='datePickBox' />
                                 </Form.Item>
 
                                 <Form.Item
                                     name="endDate"
                                     rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please select Event End Date!',
-                                    },
+                                        {
+                                            required: true,
+                                            message: 'Please select Event End Date!',
+                                        },
                                     ]}
                                 >
-                                    <DatePicker placeholderText="End Date:" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })}   className='datePickBox'/>
+                                    <DatePicker placeholderText="End Date:" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} className='datePickBox' />
                                 </Form.Item>
-                                
+
                                 <Form.Item>
-                                    <Button type='primary' htmlType = "submit" onClick={()=>{openNotification();asyncCall();}}>
+                                    <Button type='primary' htmlType="submit" onClick={() => { openNotification(); asyncCall(); }}>
                                         Create Event
                                     </Button>
-                        </Form.Item>
+                                </Form.Item>
                             </Form>
                         </Card>
                     </Drawer>
                 </div>
-                <Button className= "eventButton" type="primary" onClick={showDrawer} id='eventButton'>Add New Event</Button>
+                <Button data-tesid="eventButton" className="eventButton" type="primary" onClick={showDrawer} id='eventButton'>Add New Event</Button>
             </div>
-            
-        </div>
-        
-        
 
-      
+        </div>
+
+
+
+
     )
 }
 
--- group/src/testing/Calendar.test.js
+++ group/src/testing/Calendar.test.js
@@ -1,13 +1,29 @@
-import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react'
 import Homepage from '../components/Homepage';
+import Calendar from '../components/Calendar';
 import ReactTestUtils from 'react-dom/test-utils';
 import ReactDOM from 'react-dom';
+import { StaticRouter } from "react-router-dom";
 
-it('Get Current Day weather', () => {
-  const div = document.createElement('div');
-  render(
-    <BrowserRouter><Homepage /></BrowserRouter>
-  );
-});
+// it('Get Current Day weather', () => {
+//   const div = document.createElement('div');
+//   render(
+//     <BrowserRouter><Homepage /></BrowserRouter>
+//   );
+// });
+
+
+// Calendar test suit
+describe('Calendar', () => {
+  test('renders add new Event', () => {
+    render(<BrowserRouter><Calendar /></BrowserRouter>)
+
+    const heading = screen.getByText('Add New Event');
+
+    fireEvent.click(heading);
+
+    // expect(heading).toBeInTheDocument()
+  })
+})
