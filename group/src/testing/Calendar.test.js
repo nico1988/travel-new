44614aa748c03e90be04cf0b342a296a0dbdccc2
nico1988 <410827024@qq.com>
2022-05-15 01:59:25 +0800

    feat: Calendar test

--- group/src/testing/Calendar.test.js
+++ group/src/testing/Calendar.test.js
@@ -1,13 +1,26 @@
-import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import React from 'react';
-import Homepage from '../components/Homepage';
-import ReactTestUtils from 'react-dom/test-utils';
-import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import { render, screen, fireEvent } from '@testing-library/react'
+import Calendar from '../components/Calendar';
 
-it('Get Current Day weather', () => {
-  const div = document.createElement('div');
-  render(
-    <BrowserRouter><Homepage /></BrowserRouter>
-  );
-});
+delete window.matchMedia
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })
+
+// Calendar test suit
+describe('Calendar', () => {
+  it('renders add new Event', () => {
+    render(<BrowserRouter><Calendar /></BrowserRouter>)
+    const heading = screen.queryByText(/Add New Event/);
+    fireEvent.click(heading);
+    expect(heading).toBeInTheDocument()
+  })
+})
