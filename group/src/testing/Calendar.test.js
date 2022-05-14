232f1b89815d3eb11d725d2be4c8ff24bc8af403
nico1988 <410827024@qq.com>
2022-05-15 02:13:36 +0800

    feat: common.js

--- group/src/testing/Calendar.test.js
+++ group/src/testing/Calendar.test.js
@@ -2,18 +2,7 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { render, screen, fireEvent } from '@testing-library/react'
 import Calendar from '../components/Calendar';
-
-delete window.matchMedia
-  window.matchMedia = (query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })
+import './common.js';
 
 // Calendar test suit
 describe('Calendar', () => {
