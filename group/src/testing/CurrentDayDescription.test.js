bca02b638cdae1dae7dc55833a3ed21637f42fa6
nico1988 <410827024@qq.com>
2022-05-15 08:00:43 +0800

    feat: add calendar CurrentDayDescription.test forecaset test

--- group/src/testing/CurrentDayDescription.test.js
+++ group/src/testing/CurrentDayDescription.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-import { render, screen, fireEvent } from '@testing-library/react'
-import Calendar from '../components/Calendar';
+import { render } from '@testing-library/react'
 import Form from '../components/Form';
 import './common.js';
 import { expect, test } from "@jest/globals";
@@ -11,22 +10,27 @@ const submitSearch = (e) => {
 }
 
 // Calendar test suit
+// @ts-ignore
 describe('CurrentDayDescription component test suit', () => {
   // test suit for Form component
   test('should renders search button', async () => {
     const FormElement = render(<BrowserRouter><Form submitSearch={submitSearch} /></BrowserRouter>);
     const formSearchButton = await FormElement.findByTestId('formSearchButton');
 
+    // @ts-ignore
     expect(formSearchButton).toBeInTheDocument();
 
+    // @ts-ignore
     console.log('formSearchButton', formSearchButton.elementType);
   })
 
-  it('shoud render input', async () => {
+  // @ts-ignore
+  it('input placeholder should be placeholder', async () => {
     const FormElement = render(<BrowserRouter><Form submitSearch={submitSearch} /></BrowserRouter>);
 
     const input = await FormElement.findByTestId('formInput');
 
+    // @ts-ignore
     expect(input.placeholder).toBe('Enter location')
   })
 
